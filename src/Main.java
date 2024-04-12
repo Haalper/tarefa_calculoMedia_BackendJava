acd836aea1e7273d8cd5e10c4c838b7732cf823a
Halisson Nascimento <halissonp@hotmail.com>
2024-04-12 19:18:27 -0300

    commit inicial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String args[]) {
+        double português = 10;
+        double matemática = 7;
+        double história = 8;
+        double geografia = 5;
+
+        double soma = português + matemática + história + geografia;
+
+        double media = soma / 4;
+
+        System.out.println("Sua média é: " + media);
+    }
+}
